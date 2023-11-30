1f6384bb6f4477879ba0f19466f2901a924e45dd
yonggyo1125 <yonggyo00@kakao.com>
2023-11-30 11:01:31 +0900

    수업 소스

---
+++ 2. 서블릿 & JSP/exam02/src/main/java/exam01/Book.java
@@ -0,0 +1,12 @@
+package exam01;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Book {
+    private String title;
+    private String author;
+    private String publisher;
+}
