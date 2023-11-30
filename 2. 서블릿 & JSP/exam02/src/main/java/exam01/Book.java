314a995aabfcd3666e8d8bf4da1e7cefee8fc6ee
yonggyo1125 <yonggyo00@kakao.com>
2023-11-30 12:01:01 +0900

    수업 소스

--- 2. 서블릿 & JSP/exam02/src/main/java/exam01/Book.java
+++ 2. 서블릿 & JSP/exam02/src/main/java/exam01/Book.java
@@ -1,12 +1,20 @@
 package exam01;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
+import lombok.*;
+
+//@Getter
+//@Setter
+//@ToString
+//@NoArgsConstructor(access = AccessLevel.PRIVATE) // 기본 생성자
+//@AllArgsConstructor // 모든 멤버 변수 초기화 생성자
+//@RequiredArgsConstructor
+//@EqualsAndHashCode
+@Data // @Getter @Setter @ToString @EqualsAndHashCode
 public class Book {
     private String title;
     private String author;
     private String publisher;
+
+
 }
