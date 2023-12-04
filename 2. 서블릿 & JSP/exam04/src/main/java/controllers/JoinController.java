364bc5d78972205087893a467cc35591614bb495
yonggyo1125 <yonggyo00@kakao.com>
2023-12-04 14:37:37 +0900

    수업 소스

---
+++ 2. 서블릿 & JSP/exam04/src/main/java/controllers/JoinController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/member/join")
+public class JoinController extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.setAttribute("title", "회원가입");
+
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/templates/member/join.jsp"); // View
+        rd.forward(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
