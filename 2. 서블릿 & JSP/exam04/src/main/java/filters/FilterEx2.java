044ad05651b1cca4682a83fdf2f8cda3aba5f65a
yonggyo1125 <yonggyo00@kakao.com>
2023-12-04 15:25:42 +0900

    수업 소스

--- 2. 서블릿 & JSP/exam04/src/main/java/filters/FilterEx2.java
+++ 2. 서블릿 & JSP/exam04/src/main/java/filters/FilterEx2.java
@@ -4,10 +4,11 @@ import jakarta.servlet.*;
 
 import java.io.IOException;
 
-public class FilterEx1 implements Filter {
+public class FilterEx2 implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("FilterEx1!!!!");
+
+        System.out.println("FilterEx2!!!");
 
         chain.doFilter(request, response);
     }
