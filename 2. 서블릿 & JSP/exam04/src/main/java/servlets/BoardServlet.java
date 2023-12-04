c66970d0f670de21c4f13bd9c92ab17628b477bc
yonggyo1125 <yonggyo00@kakao.com>
2023-12-04 10:00:38 +0900

    수업 소스

--- 2. 서블릿 & JSP/exam04/src/main/java/servlets/BoardServlet.java
+++ 2. 서블릿 & JSP/exam04/src/main/java/servlets/BoardServlet.java
@@ -7,12 +7,21 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 public class BoardServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+        //resp.setCharacterEncoding("UTF-8");
+
         PrintWriter out = resp.getWriter();
         out.println("<form method='post' action='/exam04/board'>");
+        out.println("취미 : <input type='checkbox' name='hobby' value='취미1'>취미1");
+        out.println("<input type='checkbox' name='hobby' value='취미2'>취미2");
+        out.println("<input type='checkbox' name='hobby' value='취미3'>취미3");
+        out.println("<input type='checkbox' name='hobby' value='취미4'>취미4<br>");
+
         out.println("제목 : <input type='text' name='subject'><br>");
         out.println("내용 : <textarea name='content'></textarea><br>");
         out.println("<button type='submit'>작성하기</button>");
@@ -21,6 +30,11 @@ public class BoardServlet extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String subject = req.getParameter("subject");
+        String content = req.getParameter("content");
+        String[] hobby = req.getParameterValues("hobby");
 
+        System.out.printf("subject=%s, content=%s%n", subject, content);
+        System.out.printf("hobby=%s%n", Arrays.toString(hobby));
     }
 }
