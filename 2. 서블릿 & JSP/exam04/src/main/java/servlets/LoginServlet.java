9124c0f4eac31590fdbcb67ade5122c2b520afdc
yonggyo1125 <yonggyo00@kakao.com>
2023-12-04 11:54:19 +0900

    수업 소스

--- 2. 서블릿 & JSP/exam04/src/main/java/servlets/LoginServlet.java
+++ 2. 서블릿 & JSP/exam04/src/main/java/servlets/LoginServlet.java
@@ -8,10 +8,22 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/member/login")
+//@WebServlet("/member/login")
 public class LoginServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("로그인 페이지!!!");
+        System.out.println("doGet()!!!");
+
     }
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init()!!!!");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy()!!!");
+    }
+
+
 }
--- 2. 서블릿 & JSP/exam04/src/main/webapp/jspex/ex02.jsp
+++ 2. 서블릿 & JSP/exam04/src/main/webapp/jspex/ex02.jsp
@@ -1,4 +1,10 @@
 <%@ page contentType="text/html; charset=UTF-8"%>
 <%
-    response.sendRedirect("ex03.jsp");
+    // _jspService 지역 내부
+
+    //response.sendRedirect("ex03.jsp");
+    out.write("<h1>ex02.jsp");
+%>
+<%!
+    // 표현문에서는 내장 객체 접근 불가..
 %>
