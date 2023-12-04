44977ba93717bd106759885c23ec5495e8983c76
yonggyo1125 <yonggyo00@kakao.com>
2023-12-04 16:56:38 +0900

    수업 소스

--- 3. 스프링/spring_study/exam01/src/main/java/config/AppCtx.java
+++ 3. 스프링/spring_study/exam01/src/main/java/config/AppCtx.java
@@ -1,4 +1,15 @@
 package config;
 
+import models.Greeter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppCtx {
+
+    @Bean
+    public Greeter greeter() {
+        return new Greeter();
+    }
+
 }
