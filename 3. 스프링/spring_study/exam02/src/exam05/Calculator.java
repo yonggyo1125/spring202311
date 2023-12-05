6a60d3b8c5b0955dd95bf45140c08ce64f5d4f84
yonggyo1125 <yonggyo00@kakao.com>
2023-12-05 17:01:30 +0900

    수업 소스

---
+++ 3. 스프링/spring_study/exam02/src/exam05/Calculator.java
@@ -0,0 +1,13 @@
+package exam05;
+
+public interface Calculator {
+    int num = 10; // public static final
+    int add(int num1, int num2); // public abstract
+
+    default int minus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+
+}
+
--- 3. 스프링/spring_study/exam02/src/exam05/Ex01.java
+++ 3. 스프링/spring_study/exam02/src/exam05/Ex01.java
@@ -1,11 +1,13 @@
-package exam04;
+package exam05;
 
 public class Ex01 {
     public static void main(String[] args) {
         Calculator cal = new SimpleCalculator();
         int result = cal.add(10, 20);
         System.out.println(result);
-        System.out.println(cal.num);
 
+        //Calculator.num = 20;
+        int result2 = cal.minus(20, 10);
+        System.out.println(result2);
     }
 }
