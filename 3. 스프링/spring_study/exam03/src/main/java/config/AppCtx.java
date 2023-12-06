4d332ceecbcb639153421ed8dd917946b522a70a
yonggyo1125 <yonggyo00@kakao.com>
2023-12-06 11:57:05 +0900

    수업 소스

--- 3. 스프링/spring_study/exam03/src/main/java/config/AppCtx.java
+++ 3. 스프링/spring_study/exam03/src/main/java/config/AppCtx.java
@@ -1,33 +1,31 @@
-package models.member;
+package config;
 
-public class ServiceManager {
+import models.member.JoinService;
+import models.member.JoinValidator;
+import models.member.ListService;
+import models.member.MemberDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-    private static ServiceManager instance = null;
+@Configuration
+public class AppCtx {
 
-    private ServiceManager() {}
-
-    public static ServiceManager getInstance() {
-        if (instance == null) {
-            instance = new ServiceManager();
-        }
-
-        return instance;
-    }
-
+    @Bean
     public MemberDao memberDao() {
-        return new CachedMemberDao();
+        return new MemberDao();
     }
 
+    @Bean
     public JoinValidator joinValidator() {
-
         return new JoinValidator(memberDao());
     }
 
+    @Bean
     public JoinService joinService() {
-
         return new JoinService(memberDao(), joinValidator());
     }
 
+    @Bean
     public ListService listService() {
         ListService listService = new ListService();
         listService.setMemberDao(memberDao());
