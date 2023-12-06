4c8a177703d5ca8cb821c83b35e730638ef3fba7
yonggyo1125 <yonggyo00@kakao.com>
2023-12-06 09:56:36 +0900

    수업 소스

--- 3. 스프링/spring_study/exam01/src/main/java/commons/valdators/RequiredValidator.java
+++ 3. 스프링/spring_study/exam01/src/main/java/commons/valdators/RequiredValidator.java
@@ -15,4 +15,11 @@ public interface RequiredValidator {
             throw e;
         }
     }
+
+    // 참일때 예외 발생
+    default void checkFalse(boolean result, RuntimeException e) {
+        if (result) {
+            throw e;
+        }
+    }
 }
--- 3. 스프링/spring_study/exam01/src/main/java/models/member/JoinService.java
+++ 3. 스프링/spring_study/exam01/src/main/java/models/member/JoinService.java
@@ -1,7 +1,20 @@
 package models.member;
 
 public class JoinService {
-    public void join() {
 
+    private MemberDao memberDao;
+    private JoinValidator validator;
+
+    public JoinService(MemberDao memberDao, JoinValidator validator) {
+        this.memberDao = memberDao;
+        this.validator = validator;
+    }
+
+    public void join(Member member) {
+        // 데이터 검증
+        validator.validate(member);
+
+        // 데이터 추가
+        memberDao.register(member);
     }
 }
