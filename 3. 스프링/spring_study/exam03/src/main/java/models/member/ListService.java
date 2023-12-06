5a46c36183db6ac6bee87965ea8aa1364820ff10
yonggyo1125 <yonggyo00@kakao.com>
2023-12-06 16:40:23 +0900

    수업 소스

--- 3. 스프링/spring_study/exam03/src/main/java/models/member/ListService.java
+++ 3. 스프링/spring_study/exam03/src/main/java/models/member/ListService.java
@@ -5,18 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Optional;
 
 public class ListService {
 
     private MemberDao memberDao;
 
-    private Optional<DateTimeFormatter> opt;
+    private DateTimeFormatter formatter;
 
     @Autowired
-    public void setFormatter(Optional<DateTimeFormatter> opt) {
-
-        this.opt = opt;
+    public void setFormatter(DateTimeFormatter formatter) {
+        this.formatter = formatter;
     }
 
     @Autowired
@@ -28,11 +26,8 @@ public class ListService {
     public void print() {
         List<Member> members = memberDao.getList();
         for (Member member : members) {
-            DateTimeFormatter formatter = opt.orElse(null);
-            if (formatter != null) {
-                String regDtStr = formatter.format(member.getRegDt());
-                member.setRegDtStr(regDtStr);
-            }
+            String regDtStr = formatter.format(member.getRegDt());
+            member.setRegDtStr(regDtStr);
 
             System.out.println(member);
         }
