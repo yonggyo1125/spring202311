ff7e93efaa9939034c64fafd76f9f929249f992d
yonggyo1125 <yonggyo00@kakao.com>
2023-12-07 10:31:48 +0900

    수업 소스

---
+++ 3. 스프링/spring_study/exam04/src/main/java/models/member/JoinService.java
@@ -0,0 +1,16 @@
+package models.member;
+
+public class JoinService {
+
+    private MemberDao memberDao;
+    private JoinValidator validator;
+
+
+    public void join(Member member) {
+        // 데이터 검증
+        validator.validate(member);
+
+        // 데이터 추가
+        memberDao.register(member);
+    }
+}
