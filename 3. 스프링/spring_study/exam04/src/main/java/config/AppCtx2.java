8c2307ac918b2e1c40c7018bfd0b14d635cd05e1
yonggyo1125 <yonggyo00@kakao.com>
2023-12-07 15:01:45 +0900

    수업 소스

--- 3. 스프링/spring_study/exam04/src/main/java/config/AppCtx2.java
+++ 3. 스프링/spring_study/exam04/src/main/java/config/AppCtx2.java
@@ -1,13 +1,21 @@
 package config;
 
 import models.Message;
+import models.Message2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppCtx2 {
     @Bean
+    @Scope("prototype")
     public Message message() {
         return new Message();
+    }
+
+    @Bean(initMethod = "init", destroyMethod = "close")
+    public Message2 message2() {
+        return new Message2();
     }
 }
