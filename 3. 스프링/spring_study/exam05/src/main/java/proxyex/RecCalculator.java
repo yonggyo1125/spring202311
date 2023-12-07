15fed2ae7c156bc4d73b990eda15a21e55f9cd87
yonggyo1125 <yonggyo00@kakao.com>
2023-12-07 15:29:46 +0900

    수업 소스

--- 3. 스프링/spring_study/exam05/src/main/java/proxyex/RecCalculator.java
+++ 3. 스프링/spring_study/exam05/src/main/java/proxyex/RecCalculator.java
@@ -3,8 +3,8 @@ package proxyex;
 public class RecCalculator implements Calculator {
     @Override
     public long factorial(long num) {
-        if (num < 1) {
-            return 1;
+        if (num < 1L) {
+            return 1L;
         }
 
         return num * factorial(num - 1);
