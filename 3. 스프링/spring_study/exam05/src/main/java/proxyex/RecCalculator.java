72a50b0d713f4b85b5b1fb6c0ad54e0a646315c5
yonggyo1125 <yonggyo00@kakao.com>
2023-12-07 15:40:04 +0900

    수업 소스

--- 3. 스프링/spring_study/exam05/src/main/java/proxyex/RecCalculator.java
+++ 3. 스프링/spring_study/exam05/src/main/java/proxyex/RecCalculator.java
@@ -3,10 +3,16 @@ package proxyex;
 public class RecCalculator implements Calculator {
     @Override
     public long factorial(long num) {
-        if (num < 1L) {
-            return 1L;
-        }
+        long stime = System.nanoTime();
+        try {
+            if (num < 1L) {
+                return 1L;
+            }
 
-        return num * factorial(num - 1);
+            return num * factorial(num - 1);
+        } finally {
+            long etime = System.nanoTime();
+            System.out.printf("걸린시간 : %d%n", etime - stime);
+        }
     }
 }
