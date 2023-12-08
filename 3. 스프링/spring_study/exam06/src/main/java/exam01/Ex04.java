f88dd396b6fbc5318b8ea83061fa100776e0cadf
yonggyo1125 <yonggyo00@kakao.com>
2023-12-08 15:45:20 +0900

    수업 소스

--- 3. 스프링/spring_study/exam06/src/main/java/exam01/Ex04.java
+++ 3. 스프링/spring_study/exam06/src/main/java/exam01/Ex04.java
@@ -2,28 +2,24 @@ package exam01;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-public class Ex02 {
+public class Ex04 {
     public static void main(String[] args) throws ClassNotFoundException {
         Class.forName("oracle.jdbc.driver.OracleDriver");
         String url = "jdbc:oracle:thin:@localhost:1521:orcl";
         String username = "SPRING6";
         String password = "_aA123456";
 
-
+        String sql= "INSERT INTO MEMBER (USER_NO, USER_ID, USER_PW, USER_NM, EMAIL) " +
+                    " VALUES (SEQ_MEMBER.nextval, ?, ?, ?, ?)";
         try (Connection conn = DriverManager.getConnection(url, username, password);
-            Statement stmt = conn.createStatement()) {
-            String userId = "USER01";
-            String sql = "INSERT INTO MEMBER (USER_NO, USER_ID, USER_PW, USER_NM, EMAIL) " +
-                    " VALUES (SEQ_MEMBER.nextval, '" + userId + "', '123456', '사용자01', 'user01@test.org')";
-
-            int result = stmt.executeUpdate(sql);
-            System.out.println(result);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 }
