31a60d5e379eedc3a420587cc6af93af71e95340
yonggyo1125 <yonggyo00@kakao.com>
2023-12-08 10:00:49 +0900

    수업 소스

--- 3. 스프링/spring_study/exam05/src/main/java/config/ProxyCache.java
+++ 3. 스프링/spring_study/exam05/src/main/java/config/ProxyCache.java
@@ -5,8 +5,13 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Aspect
 public class ProxyCache {
+
+    private Map<Long, Long> cacheData = new HashMap<>();
 
     @Pointcut("execution(* aopex..*(long))")
     public void publicTarget() {}
@@ -16,6 +21,9 @@ public class ProxyCache {
 
         Object[] args = joinPoint.getArgs(); // 매개변수로 투입된 인자 값( 예 - 10L)
         Long num = (Long)args[0];
+        if (cacheData.containsKey(num)) {
+            return cacheData.get(num);
+        }
 
         Object result = joinPoint.proceed();
 
