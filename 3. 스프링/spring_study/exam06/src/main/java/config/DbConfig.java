c01cab3b166339999391fcea615345a6a0a73323
yonggyo1125 <yonggyo00@kakao.com>
2023-12-11 10:28:59 +0900

    수업 소스

--- 3. 스프링/spring_study/exam06/src/main/java/config/DbConfig.java
+++ 3. 스프링/spring_study/exam06/src/main/java/config/DbConfig.java
@@ -3,6 +3,7 @@ package config;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class DbConfig {
@@ -14,6 +15,17 @@ public class DbConfig {
         ds.setUsername("SPRING6");
         ds.setPassword("_aA123456");
 
+        // 커넥션 풀 설정
+        ds.setInitialSize(2);
+        ds.setTestWhileIdle(true); // 유휴 상태 커넥션 객체를 체크 여부
+        ds.setTimeBetweenEvictionRunsMillis(3000); // 3초마다 커넥션 상태 체크
+        ds.setMinEvictableIdleTimeMillis(30 * 1000); // 30초가 최대 유휴 시간 -> 경과시 소멸
+
         return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
     }
 }
--- 3. 스프링/spring_study/exam06/src/test/java/jdbctest/JdbcTemplateTest.java
+++ 3. 스프링/spring_study/exam06/src/test/java/jdbctest/JdbcTemplateTest.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes= AppCtx.class)
 public class JdbcTemplateTest {
@@ -19,6 +22,11 @@ public class JdbcTemplateTest {
     @Test
     @DisplayName("DataSource를 통한 DB 연결 테스트")
     void connectionTest() {
-        
+        try (Connection conn = dataSource.getConnection()) {
+            System.out.println(conn);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
