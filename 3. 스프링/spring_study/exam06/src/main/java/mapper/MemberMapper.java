a47bcfaeb0860d10d0f9d8e8c204aa0fd51dec07
yonggyo1125 <yonggyo00@kakao.com>
2023-12-11 15:29:50 +0900

    수업 소스

--- 3. 스프링/spring_study/exam06/src/main/java/mapper/MemberMapper.java
+++ 3. 스프링/spring_study/exam06/src/main/java/mapper/MemberMapper.java
@@ -1,6 +1,7 @@
 package mapper;
 
 import models.member.Member;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -9,4 +10,7 @@ public interface MemberMapper {
 
     @Select("SELECT * FROM MEMBER")
     List<Member> getMembers();
+
+    @Insert("INSERT INTO MEMBER (USER_NO, USER_ID, USER_PW, USER_NM) VALUES (SEQ_MEMBER.nextval, #{userId}, #{userPw}, #{userNm})")
+    int register(Member member);
 }
---
+++ 3. 스프링/spring_study/exam06/src/test/java/mybatistest/MemberMapperTest.java
@@ -0,0 +1,32 @@
+package mybatistest;
+
+import config.AppCtx;
+import mapper.MemberMapper;
+import models.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = AppCtx.class)
+public class MemberMapperTest {
+
+    @Autowired
+    private MemberMapper mapper;
+    
+    @Test
+    @DisplayName("회원 목록 조회 테스트")
+    void memberListTest() {
+        List<Member> members = mapper.getMembers();
+        for (Member member : members) {
+            System.out.println(member);
+        }
+
+        System.out.println(mapper.getClass().getName());
+    }
+}
