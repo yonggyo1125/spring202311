2f36aa1fd8a57e7780334dff2b80a222137185e2
yonggyo1125 <yonggyo00@kakao.com>
2023-12-12 11:02:34 +0900

    수업 소스

--- 3. 스프링/spring_study/exam07/src/main/java/controllers/HelloController.java
+++ 3. 스프링/spring_study/exam07/src/main/java/controllers/HelloController.java
@@ -2,13 +2,27 @@ package controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
+    /*
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(@RequestParam("name") String name) {
+        System.out.println(name);
+        return "hello"; // /WEB-INF/templates/hello.jsp
+    }
+     */
 
-        return "hello";
+    @GetMapping("/hello")
+    public ModelAndView hello() {
+        ModelAndView mv = new ModelAndView();
+        // request.setAttribute("message", "안녕하세요.");
+        mv.addObject("message", "안녕하세요.");
+        mv.setViewName("hello"); // /WEB-INF/templates/hello.jsp
+
+        return mv;
     }
 }
