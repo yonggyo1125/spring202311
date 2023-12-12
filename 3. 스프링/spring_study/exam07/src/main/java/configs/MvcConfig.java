c71f220cc2d8c35f2b8c4e14e25ce91016c6c06b
yonggyo1125 <yonggyo00@kakao.com>
2023-12-12 15:48:58 +0900

    수업 소스

--- 3. 스프링/spring_study/exam07/src/main/java/configs/MvcConfig.java
+++ 3. 스프링/spring_study/exam07/src/main/java/configs/MvcConfig.java
@@ -1,10 +1,7 @@
 package configs;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
@@ -12,7 +9,14 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+        // 모든 요청 -> 컨트롤러 빈, 없는 경우 -> 정적 자원 경로(css, js, 이미지)
 
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**") // 모든 경로
+                .addResourceLocations("classpath:/static/");
     }
 
     @Override
