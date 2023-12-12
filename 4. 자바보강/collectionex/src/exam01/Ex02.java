c55306930a00c8d7e08c5ce7c1963ab3527b8713
yonggyo1125 <yonggyo00@kakao.com>
2023-12-12 17:00:56 +0900

    수업 소스

---
+++ 4. 자바보강/collectionex/src/exam01/Ex02.java
@@ -0,0 +1,28 @@
+package exam01;
+
+import java.util.ArrayList;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("이름1");
+        names.add("이름2");
+        names.add("이름3");
+        names.add("이름4");
+        names.add("이름5");
+
+        //System.out.println(names);
+
+        for (int i = 0; i < names.size(); i++) {
+            names.remove(i);
+        }
+
+
+        for (int i = 0; i < names.size(); i++) {
+            String name = names.get(i);
+            System.out.println(name);
+        }
+
+    }
+}
