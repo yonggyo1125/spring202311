bef661ad6814682c4413cb32605ccc6f9d725ec9
yonggyo1125 <yonggyo00@kakao.com>
2023-12-13 10:24:53 +0900

    수업 소스

--- 4. 자바보강/collectionex/src/exam03/Ex03.java
+++ 4. 자바보강/collectionex/src/exam03/Ex03.java
@@ -1,22 +1,24 @@
 package exam03;
 
+import java.util.Comparator;
 import java.util.TreeSet;
 
 public class Ex03 {
     public static void main(String[] args) {
-        TreeSet<Integer> numbers = new TreeSet<>();
+
+        Comparator<Integer> comp = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        };
+
+        TreeSet<Integer> numbers = new TreeSet<>(comp);
         numbers.add(10);
         numbers.add(5);
         numbers.add(33);
         numbers.add(3);
 
         System.out.println(numbers);
-    }
-}
-
-class Integer2 extends Integer {
-
-    public Integer2(int value) {
-        super(value);
     }
 }
