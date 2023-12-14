51fffbed19f8ae6b0a9b19bf88764f8f338f2cae
yonggyo1125 <yonggyo00@kakao.com>
2023-12-14 09:41:40 +0900

    수업 소스

--- 3. 스프링/spring_study/exam07/src/main/java/controllers/member/MemberController.java
+++ 3. 스프링/spring_study/exam07/src/main/java/controllers/member/MemberController.java
@@ -1,14 +1,29 @@
 package controllers.member;
 
+import models.member.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class MemberController {
 
     @GetMapping("/member/join")
-    public String join() {
+    public String join(Model model) {
+
+        Member member = Member.builder()
+                .userNo(1L)
+                .userPw("1234")
+                .userId("user01")
+                .userNm("사용자01")
+                .email("user01@test.org")
+                .regDt(LocalDateTime.now())
+                .build();
+
+        model.addAttribute("member", member);
 
         return "member/join";
     }
