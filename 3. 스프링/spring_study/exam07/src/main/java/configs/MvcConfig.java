c86cf9790f96c91401a717bb5594ee90266f5a6d
yonggyo1125 <yonggyo00@kakao.com>
2023-12-14 10:00:50 +0900

    수업 소스

--- 3. 스프링/spring_study/exam07/src/main/java/configs/MvcConfig.java
+++ 3. 스프링/spring_study/exam07/src/main/java/configs/MvcConfig.java
@@ -3,8 +3,10 @@ package configs;
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.*;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -66,5 +68,10 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.viewResolver(thymeleafViewResolver());
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
     }
 }
