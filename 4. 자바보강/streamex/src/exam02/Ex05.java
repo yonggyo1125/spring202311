9c9264ab2df00d84231a7b3d629230d5be1d78cd
yonggyo1125 <yonggyo00@kakao.com>
2023-12-18 17:01:16 +0900

    수업소스

--- 4. 자바보강/streamex/src/exam02/Ex05.java
+++ 4. 자바보강/streamex/src/exam02/Ex05.java
@@ -7,5 +7,8 @@ public class Ex05 {
     public static void main(String[] args) {
         int[] nums = IntStream.iterate(10, x -> x + 2).limit(10).toArray();
         System.out.println(Arrays.toString(nums));
+
+        int[] nums2 = IntStream.generate(() -> 1).limit(10).toArray();
+        System.out.println(Arrays.toString(nums2));
     }
 }
