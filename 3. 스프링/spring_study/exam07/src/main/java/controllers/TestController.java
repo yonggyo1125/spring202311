21df7870fe9602387ef7ad1ce4941b33abae542f
yonggyo1125 <yonggyo00@kakao.com>
2023-12-18 14:25:33 +0900

    수업 소스

--- 3. 스프링/spring_study/exam07/src/main/java/controllers/TestController.java
+++ 3. 스프링/spring_study/exam07/src/main/java/controllers/TestController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-    @GetMapping("/test1")
-    public String test1(HttpServletResponse response) {
+    @GetMapping("/cookie/test1")
+    public String test1(HttpServletResponse response, HttpServletRequest request) {
+        Cookie cookie = new Cookie("key1", "value1");
+        response.addCookie(cookie);
+
+        Cookie cookie2 = new Cookie("key2", "value2");
+        cookie2.setPath(request.getContextPath()); // /exam07
+        response.addCookie(cookie);
+
+        return "test";
+    }
+
+    @GetMapping("/cookie1/test2")
+    public String test2() {
 
         return "test";
     }
