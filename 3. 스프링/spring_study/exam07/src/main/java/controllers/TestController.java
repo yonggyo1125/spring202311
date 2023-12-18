7e77a1909c1bc948df5209b3ad278e1f4957480a
yonggyo1125 <yonggyo00@kakao.com>
2023-12-18 14:58:41 +0900

    수업 소스

--- 3. 스프링/spring_study/exam07/src/main/java/controllers/TestController.java
+++ 3. 스프링/spring_study/exam07/src/main/java/controllers/TestController.java
@@ -3,6 +3,7 @@ package controllers;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -16,7 +17,12 @@ public class TestController {
 
         Cookie cookie2 = new Cookie("key2", "value2");
         cookie2.setPath(request.getContextPath()); // /exam07
-        response.addCookie(cookie);
+        cookie2.setMaxAge(60 * 60 * 24 * 7);
+        //cookie2.setMaxAge(0); // 1970.1.1 자정 - 쿠키 삭제
+        cookie2.setHttpOnly(true); // false - 자바스크립트로 브라우저에서 조회 가능 
+                                // true - 자바스크립트 조회 X, 서버쪽에서만 요청을 통한 조회 가능
+        
+        response.addCookie(cookie2);
 
         return "test";
     }
@@ -24,6 +30,20 @@ public class TestController {
     @GetMapping("/cookie1/test2")
     public String test2() {
 
+        return "test";
+    }
+
+    @GetMapping("/session/test1")
+    public String test3(HttpSession session) {
+        session.setAttribute("key1", "value1");
+
+        return "test";
+    }
+
+    @GetMapping("/session/test2")
+    public String test4(HttpSession session) {
+        String value = (String)session.getAttribute("key1");
+        System.out.printf("key1=%s%n", value);
         return "test";
     }
 }
