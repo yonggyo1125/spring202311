baf72dfc53057d31a25362a738a70eb568dc42d4
yonggyo1125 <yonggyo00@kakao.com>
2023-12-18 11:27:02 +0900

    수업 소스

--- 3. 스프링/spring_study/exam07/src/main/java/models/member/JoinService.java
+++ 3. 스프링/spring_study/exam07/src/main/java/models/member/JoinService.java
@@ -2,6 +2,7 @@ package models.member;
 
 import controllers.member.RequestJoin;
 import lombok.RequiredArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +12,11 @@ public class JoinService {
 
     public void join(RequestJoin form) {
 
+        String hash = BCrypt.hashpw(form.getUserPw(), BCrypt.gensalt(12));
+
         Member member = Member.builder()
                 .userId(form.getUserId())
-                .userPw(form.getUserPw())
+                .userPw(hash)
                 .userNm(form.getUserNm())
                 .email(form.getEmail())
                 .build();
