9f142f8e37afa130be35748cd9a01abb7ac66072
yonggyo1125 <yonggyo00@kakao.com>
2023-12-18 10:25:46 +0900

    수업 소스

--- 3. 스프링/spring_study/exam07/src/main/java/mapper/MemberMapper.java
+++ 3. 스프링/spring_study/exam07/src/main/java/mapper/MemberMapper.java
@@ -1,4 +1,10 @@
 package mapper;
 
+import models.member.Member;
+
 public interface MemberMapper {
+    int register(Member member);
+    int exist(String userId);
+
+    Member get(String userId);
 }
---
+++ 3. 스프링/spring_study/exam07/src/main/java/models/member/MemberDao.java
@@ -0,0 +1,30 @@
+package models.member;
+
+import lombok.RequiredArgsConstructor;
+import mapper.MemberMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberDao {
+
+    private final MemberMapper mapper;
+
+
+    public boolean register(Member member) {
+
+        int affectedRows = mapper.register(member);
+
+        return affectedRows > 0;
+    }
+
+    public boolean exist(String userId) {
+        int cnt = mapper.exist(userId);
+
+        return cnt > 0; // 1이상 - 이미 등록된 아이디
+    }
+
+    public Member get(String userId) {
+        return mapper.get(userId);
+    }
+}
