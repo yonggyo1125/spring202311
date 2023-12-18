080a23be48169fc4ed4e86cfdb682bb0602d2a92
yonggyo1125 <yonggyo00@kakao.com>
2023-12-18 09:50:37 +0900

    수업 소스

--- 3. 스프링/spring_study/exam07/src/test/java/tests/JoinServiceTest.java
+++ 3. 스프링/spring_study/exam07/src/test/java/tests/JoinServiceTest.java
@@ -1,15 +1,20 @@
 package tests;
 
+import configs.DbConfig;
 import configs.MvcConfig;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import java.sql.Connection;
+
 @SpringJUnitConfig
-@ContextConfiguration(classes = MvcConfig.class)
+@ContextConfiguration(classes = DbConfig.class)
 public class JoinServiceTest {
 
     @Autowired
@@ -18,6 +23,10 @@ public class JoinServiceTest {
     @Test
     @DisplayName("데이터베이스 연결 테스트")
     void connectionTest() {
-        
+        try (Connection conn = dataSrouce.getConnection()) {
+            System.out.println(conn);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
