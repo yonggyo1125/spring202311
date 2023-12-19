d62c5fb5755f95067545fab6cf520d207cb9760d
yonggyo1125 <yonggyo00@kakao.com>
2023-12-19 10:34:35 +0900

    수업 소스

--- 3. 스프링/spring_study/exam07/src/main/java/controllers/member/LoginValidator.java
+++ 3. 스프링/spring_study/exam07/src/main/java/controllers/member/LoginValidator.java
@@ -1,9 +1,20 @@
 package controllers.member;
 
+import lombok.RequiredArgsConstructor;
+import models.member.Member;
+import models.member.MemberDao;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+@Component
+@RequiredArgsConstructor
 public class LoginValidator implements Validator {
+
+    private final MemberDao memberDao;
+
     @Override
     public boolean supports(Class<?> clazz) {
         return clazz.isAssignableFrom(RequestLogin.class);
@@ -15,6 +26,27 @@ public class LoginValidator implements Validator {
          * 1. 아이디로 회원이 조회 되는지?
          * 2. 조회된 회원의 비밀번호와 입력한 비밀번호의 일치 여부
          */
+
+        RequestLogin form = (RequestLogin)target;
+
+        String userId = form.userId();
+        String userPw = form.userPw();
+
+        //아이디로 회원이 조회 되는지?
+        Member member = memberDao.get(userId);
+        if (member == null) {
+            errors.reject("Login.auth.fail");
+        }
+
+        // 조회된 회원의 비밀번호와 입력한 비밀번호의 일치 여부
+        if (member != null && StringUtils.hasText(userPw) &&
+            !BCrypt.checkpw(userPw, member.getUserPw())) {
+            errors.reject("Login.auth.fail");
+        }
+
+
+
+
 
     }
 }
