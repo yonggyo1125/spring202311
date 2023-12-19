7d152a63a55a313b6f15c673b8130a363ddac2be
yonggyo1125 <yonggyo00@kakao.com>
2023-12-19 14:48:15 +0900

    수업 소스

--- 3. 스프링/spring_study/exam07/src/main/java/controllers/admin/MemberController.java
+++ 3. 스프링/spring_study/exam07/src/main/java/controllers/admin/MemberController.java
@@ -5,6 +5,7 @@ import models.member.Member;
 import models.member.MemberDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +20,10 @@ public class MemberController {
     private final MemberDao memberDao;
 
     @GetMapping  // /admin/member
-    public String index(@ModelAttribute MemberSearch search, Model model) {
+    public String index(@ModelAttribute MemberSearch search, Errors errors, Model model) {
 
-        List<Member> members = memberDao.getList(search);
-        model.addAttribute("members", members);
+        //List<Member> members = memberDao.getList(search);
+        //model.addAttribute("members", members);
 
         return "admin/member/list";
     }
--- 3. 스프링/spring_study/exam07/src/main/java/controllers/admin/MemberSearch.java
+++ 3. 스프링/spring_study/exam07/src/main/java/controllers/admin/MemberSearch.java
@@ -1,11 +1,15 @@
 package controllers.admin;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Data
 public class MemberSearch {
+    @DateTimeFormat(pattern="yyyy/MM/dd")
     private LocalDate sdate;
+
+    @DateTimeFormat(pattern="yyyy/MM/dd")
     private LocalDate edate;
 }
