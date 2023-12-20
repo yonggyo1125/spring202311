9b04a99f16fbec76b57a7c31383bbf97ec231cd7
yonggyo1125 <yonggyo00@kakao.com>
2023-12-20 10:00:56 +0900

    수업 소스

--- 3. 스프링/spring_study/exam07/src/main/java/controllers/CommonController.java
+++ 3. 스프링/spring_study/exam07/src/main/java/controllers/CommonController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import commons.CommonException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,6 +9,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class CommonController {
     @ExceptionHandler(Exception.class)
     public String errorHandler(Exception e, Model model) {
+
+        if (e instanceof CommonException) {
+
+        }
 
         e.printStackTrace();
 
