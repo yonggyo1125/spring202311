b82cfb0f68b127d2a80a9e1017b21cb85172c37c
yonggyo1125 <yonggyo00@kakao.com>
2023-12-21 10:00:10 +0900

    수업 소스

--- 5.스프링부트/exam01/src/main/java/org/choongang/configs/MvcConfig.java
+++ 5.스프링부트/exam01/src/main/java/org/choongang/configs/MvcConfig.java
@@ -1,9 +1,23 @@
 package org.choongang.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableConfigurationProperties(FileProperties.class)
 public class MvcConfig implements WebMvcConfigurer {
 
+    @Autowired
+    private FileProperties fileProperties;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler(fileProperties.getUrl() + "**")
+                .addResourceLocations("file:///" + fileProperties.getPath());
+
+    }
 }
