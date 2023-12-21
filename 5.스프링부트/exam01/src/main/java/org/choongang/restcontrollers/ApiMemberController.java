f3b3f2cc6cbd8cfaaa4a2ad57cbef80de0824e28
yonggyo1125 <yonggyo00@kakao.com>
2023-12-21 14:48:05 +0900

    수업 소스

---
+++ 5.스프링부트/exam01/src/main/java/org/choongang/restcontrollers/ApiMemberController.java
@@ -0,0 +1,28 @@
+package org.choongang.restcontrollers;
+
+import org.choongang.entities.Member;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/api/member")
+public class ApiMemberController {
+
+    @GetMapping
+    public Member info() {
+        Member member = Member.builder()
+                .userNo(1L)
+                .userPw("12345678")
+                .userId("user01")
+                .userNm("사용자01")
+                .email("user01@test.org")
+                .regDt(LocalDateTime.now())
+                .modDt(LocalDateTime.now())
+                .build();
+
+        return member;
+    }
+}
