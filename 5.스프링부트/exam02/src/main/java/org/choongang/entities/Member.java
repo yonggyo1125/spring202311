b2f9f53c8724d09be25ea0824d44108ece72f898
yonggyo1125 <yonggyo00@kakao.com>
2023-12-22 16:01:42 +0900

    수업 소스

---
+++ 5.스프링부트/exam02/src/main/java/org/choongang/entities/Member.java
@@ -0,0 +1,19 @@
+package org.choongang.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Member {
+    @Id
+    private Long seq;
+    private String email;
+    private String name;
+    private String password;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+}
