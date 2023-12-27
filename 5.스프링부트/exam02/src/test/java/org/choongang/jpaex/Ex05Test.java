4c021c36ba348348fa7a0d6ef5af4c052aca4cbd
yonggyo1125 <yonggyo00@kakao.com>
2023-12-27 09:15:15 +0900

    수업 소스

---
+++ 5.스프링부트/exam02/src/test/java/org/choongang/jpaex/Ex05Test.java
@@ -0,0 +1,32 @@
+package org.choongang.jpaex;
+
+import org.choongang.entities.BoardData;
+import org.choongang.repositories.BoardDataRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+@TestPropertySource(properties = "spring.profiles.active=test")
+public class Ex05Test {
+
+    @Autowired
+    private BoardDataRepository repository;
+
+    @BeforeEach
+    void init() {
+        List<BoardData> items = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            BoardData item = new BoardData();
+            item.setSubject("제목" + i);
+            item.setContent("내용" + i);
+            items.add(item);
+        }
+
+        repository.saveAllAndFlush(items);
+    }
+}
