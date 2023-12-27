bf46dd6597310d5f643d046a78f99abf11ea1b6f
yonggyo1125 <yonggyo00@kakao.com>
2023-12-27 10:43:11 +0900

    수업 소스

--- 5.스프링부트/exam02/src/main/java/org/choongang/entities/Member.java
+++ 5.스프링부트/exam02/src/main/java/org/choongang/entities/Member.java
@@ -14,9 +14,6 @@ import java.util.Date;
 
 @Data
 @Entity
-@Table(name="USERS",
-        indexes = @Index(name="idx_member_createdAt", columnList = "createdAt DESC"))
-//@EntityListeners(AuditingEntityListener.class)
 public class Member extends Base {
     @Id @GeneratedValue
     private Long seq;
@@ -27,26 +24,11 @@ public class Member extends Base {
     @Column(length=40, nullable = false)
     private String name;
 
-    @Column(length=65, name="userPw", nullable = false)
+    @Column(length=65, nullable = false)
     private String password; // varchar2
-    //@Lob
-    @Transient
-    private String introduction; // CLOB
 
     @Enumerated(EnumType.STRING)
     @Column(length=10)
     private MemberType type;
-    /*
-   // @CreationTimestamp // INSERT SQL 실행시
-    @CreatedDate
-    private LocalDateTime createdAt;
 
-   // @UpdateTimestamp // UPDATE SQL 실행시
-    @LastModifiedDate
-    private LocalDateTime modifiedAt;
-       */
-    //@Temporal(TemporalType.DATE) // 날짜
-   // @Temporal(TemporalType.TIME) // 시간
-    //@Temporal(TemporalType.TIMESTAMP) // 날짜 + 시간
-    //public Date dt;
 }
