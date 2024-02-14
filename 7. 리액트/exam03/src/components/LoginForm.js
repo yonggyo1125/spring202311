419cf2519485bd7974cbf325de9e7b683b32a534
yonggyo1125 <yonggyo00@kakao.com>
2024-02-14 12:02:05 +0900

    .

--- 7. 리액트/exam03/src/components/LoginForm.js
+++ 7. 리액트/exam03/src/components/LoginForm.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MediumButton as LoginButton } from './commons/StyledButton';
+import { FiLogIn, FiSquare, FiCheckSquare } from 'react-icons/fi';
 
 const LoginForm = () => {
+  const [saveId, setSaveId] = useState(false);
+  const onToggle = useCallback(() => setSaveId((saveId) => !saveId), []);
   return (
     <>
       <h1>로그인</h1>
       <form>
         <input type="text" name="userId" placeholder="아이디" />
         <input type="password" name="password" placeholder="비밀번호" />
+
+        <div onClick={onToggle}>
+          {saveId ? <FiCheckSquare /> : <FiSquare />} 아이디 저장
+        </div>
+
         <LoginButton type="submit" bcolor="primary" color="#fff">
+          <FiLogIn />
           로그인
         </LoginButton>
       </form>
--- 7. 리액트/exam03/src/components/commons/StyledButton.js
+++ 7. 리액트/exam03/src/components/commons/StyledButton.js
@@ -1,9 +1,15 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { buttonSizes, fontSizes } from '../../styles/size';
 import color from '../../styles/color';
 
 const { small, medium, big } = buttonSizes;
 const { small: fSmall, medium: fMedium, big: fBig } = fontSizes;
+
+const commonStyle = css`
+  border: 0;
+  cursor: pointer;
+  border-radius: 5px;
+`;
 
 export const SmallButton = styled.button`
   width: ${small.width}px;
@@ -11,6 +17,7 @@ export const SmallButton = styled.button`
   font-size: ${fSmall}rem;
   background: ${({ bcolor }) => color[bcolor] || bcolor};
   color: ${({ color }) => color[color] || color};
+  ${commonStyle}
 `;
 
 export const MediumButton = styled.button`
@@ -19,6 +26,11 @@ export const MediumButton = styled.button`
   font-size: ${fMedium}rem;
   background: ${({ bcolor }) => color[bcolor] || bcolor};
   color: ${({ color }) => color[color] || color};
+  ${commonStyle}
+
+  ${({active}) => active && css`
+    border:2px solid #000;
+  `}
 `;
 
 export const BigButton = styled.button`
@@ -27,4 +39,5 @@ export const BigButton = styled.button`
   font-size: ${fBig}rem;
   background: ${({ bcolor }) => color[bcolor] || bcolor};
   color: ${({ color }) => color[color] || color};
+  ${commonStyle}
 `;
